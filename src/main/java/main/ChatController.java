c0243244d5b95bc8945f631bb4ffe62f2b7e629b
Sergey Galichev <sergey.galichev@outlook.com>
2023-03-24 11:12:52 +1200

    initial commit. created entities, chat controller, application main class, application settings file and frontend

---
+++ src/main/java/main/ChatController.java
@@ -0,0 +1,35 @@
+package main;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class ChatController {
+
+    @GetMapping("/init")
+    public String init() {
+        //TODO: check sessionId. If found => true, if not - false
+
+        return "YES";
+    }
+
+    @PostMapping("/message")
+    public Boolean sendMessage(@RequestParam String message) {
+        return true;
+    }
+
+    @GetMapping("/message")
+    public List<String> getMessagesList() {
+        return null;
+    }
+
+    @GetMapping("/user")
+    public HashMap<Integer, String> getUsersList() {
+        return null;
+    }
+}
